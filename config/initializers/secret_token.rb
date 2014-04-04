c41b9800438c1a594af8740a32b97903e71947a8
Drew Butler <abutler3@gmail.com>
2014-04-03 20:23:07 -0500

    first commit-Add bootstrap style and listing model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EtsyClone::Application.config.secret_key_base = 'b1019d0fe5d0a0fd1812ff91f0100132e3375e825a918395378a10e02bc19fb39b710a1b81d834f37f114c91e12bbec8dc8ced3ec6b732c72fdbe675e07d9a11'
